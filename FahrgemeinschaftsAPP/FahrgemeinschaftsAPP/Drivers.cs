5ed834c41d69931b92e73afeba125841824a9505
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-29 09:10:50 +0200

    beginning + login

---
+++ FahrgemeinschaftsAPP/FahrgemeinschaftsAPP/Drivers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FahrgemeinschaftsAPP
+{
+    public class Drivers
+    {
+        string VorName { get; set; }
+        string NachName { get; set; }
+
+        public Drivers(string vorName, string nachName)
+        {
+            NachName = nachName;
+            VorName = vorName;
+        }
+
+
+    }
+}
---
+++ FahrgemeinschaftsAPP/FahrgemeinschaftsAPP/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FahrgemeinschaftsAPP
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            //Wahl zwischen login und registrieren
+            Console.WriteLine("Please choose a option: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[1] = Login ");
+            Console.WriteLine("[2] = Register ");
+            Console.ForegroundColor = ConsoleColor.White;
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+
+            //LOGIN
+            if (userChoice == 1)
+            {
+                string username = "root";
+                string password = "root";
+
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter your Username: ");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                //benutzernamen überprüfen
+                string usrName = Console.ReadLine();
+                if (usrName == username)
+                {
+                    //Password überprüfen
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please enter your Password: : ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string usrPass = Console.ReadLine();
+                    if (usrPass == password)
+                    {
+
+                        //Willkommens screen + optionen
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("    :::       ::: :::::::::: :::        ::::::::   ::::::::    :::   :::   ::::::::::");
+                        Console.WriteLine("   :+:       :+: :+:        :+:       :+:    :+: :+:    :+:  :+:+: :+:+:  :+:        ");
+                        Console.WriteLine("  +:+ +:+ +:+ +:+ +:+ +:+ +:+ +:+ +:+:+ +:+ +:+                                      ");
+                        Console.WriteLine(" +#+  +:+  +#+ +#++:++#   +#+       +#+        +#+    +:+ +#+  +:+  +#+ +#++:++#     ");
+                        Console.WriteLine("+#+ +#+#+ +#+ +#+        +#+       +#+        +#+    +#+ +#+       +#+ +#+           ");
+                        Console.WriteLine("#+#+# #+#+#  #+#        #+#       #+#    #+# #+#    #+# #+#       #+# #+#            ");
+                        Console.WriteLine("###   ###   ########## ########## ########   ########  ###       ### ##########      ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"                            Welcome {username}!");
+
+                        Console.WriteLine("[1] Add Driver");
+                        int UC = Convert.ToInt32(Console.ReadLine());
+
+                        //Opionen auswählen
+                        if (UC == 1)
+                        {
+                            Console.Clear();
+
+                            //Liste der fahrer
+                            List<Drivers> drivers = new List<Drivers>();
+
+
+                            //Hinzugefügten Fahrer ausgeben
+                            
+                        }
+
+                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong password or username please try again! ");
+                }
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
